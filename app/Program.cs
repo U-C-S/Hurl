1d4c43bef0adf84c086e98c2b22407435a550dd7
U-C-S <ucs3721@outlook.com>
2021-08-04 17:59:56 +0530

    Improved folder structure

---
+++ app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RuntimeInformation.OSDescription + " " + RuntimeInformation.OSArchitecture);
+
+            browserlist.getlist();
+            // Console.WriteLine(x);
+            // Console.WriteLine(RuntimeInformation.FrameworkDescription);
+        }
+    }
+}
