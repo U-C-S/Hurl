e24ef857708d2b3970303e1b79274095b634a6ea
Chanakya <ucs3721@outlook.com>
2021-10-19 20:08:05 +0530

    Support for Hiding Browsers (through editing settings file)

--- App/Hurl.BrowserSelector/MainWindow.xaml.cs
+++ App/Hurl.BrowserSelector/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using Hurl.SharedLibraries.Models;
 using Hurl.SharedLibraries.Services;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 
@@ -29,32 +30,31 @@ namespace Hurl.BrowserSelector
 
         private void Window_Loaded()
         {
-            List<Browser> x = new SettingsFile().SettingsObject.Browsers;
+            //var y = from z in x where z.ExePath is not null and z.Hidden is false select z;
+            IEnumerable<Browser> LoadableBrowsers = from b in SettingsFile.LoadNewInstance().SettingsObject.Browsers
+                                                    where b.Name != null && b.ExePath != null && b.Hidden != true
+                                                    select b;
 
-            foreach (Browser i in x)
+            foreach (Browser i in LoadableBrowsers)
             {
-                if (i.Name != null)
+                BrowserIconBtn browserUC = new BrowserIconBtn()
                 {
-                    BrowserIconBtn browserUC = new BrowserIconBtn()
-                    {
-                        BrowserName = i.Name,
-                        BrowserIcon = i.GetIcon,
-                        ExePath = i.ExePath,
-                        URL = OpenedLink + " " + i.LaunchArgs
-                    };
+                    BrowserName = i.Name,
+                    BrowserIcon = i.GetIcon,
+                    ExePath = i.ExePath,
+                    URL = OpenedLink + " " + i.LaunchArgs
+                };
 
-                    var separator = new System.Windows.Shapes.Rectangle()
-                    {
-                        Width = 1,
-                        Height = 40,
-                        Margin = new Thickness(3, 0, 3, 20),
-                        Fill = System.Windows.Media.Brushes.AliceBlue
-                    };
+                var separator = new System.Windows.Shapes.Rectangle()
+                {
+                    Width = 1,
+                    Height = 40,
+                    Margin = new Thickness(3, 0, 3, 20),
+                    Fill = System.Windows.Media.Brushes.AliceBlue
+                };
 
-                    _ = stacky.Children.Add(browserUC);
-                    _ = stacky.Children.Add(separator);
-                }
-
+                _ = stacky.Children.Add(browserUC);
+                _ = stacky.Children.Add(separator);
             }
 
             stacky.Children.RemoveAt(stacky.Children.Count - 1);
