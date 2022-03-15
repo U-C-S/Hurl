9347a86fb81a6c04b2bd6cdb3ed2fee8ea788c80
U-C-S <ucs3721@outlook.com>
2022-03-15 19:56:15 +0530

    Polish `MainWindow` and Create `Helpers.RoundedCorners`

--- App/Hurl.BrowserSelector/MainWindow.xaml.cs
+++ App/Hurl.BrowserSelector/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Hurl.BrowserSelector.Controls;
+using Hurl.BrowserSelector.Helpers;
 using Hurl.SharedLibraries.Constants;
 using Hurl.SharedLibraries.Models;
 using Hurl.SharedLibraries.Services;
@@ -6,11 +7,9 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Interop;
-using System.Windows.Shell;
+using WPFUI.Background;
 
 namespace Hurl.BrowserSelector
 {
@@ -23,34 +22,17 @@ namespace Hurl.BrowserSelector
 
         public MainWindow()
         {
-            WPFUI.Background.Manager.Apply(WPFUI.Background.BackgroundType.Acrylic, this);
+            Manager.Apply(BackgroundType.Acrylic, this);
 
             InitializeComponent();
 
-            //this.Activated += MainWindow_Activated;
+            RoundedCorners.Apply(this, () => WindowBorder.CornerRadius = new CornerRadius(0));
 
-            if (Environment.OSVersion.Version.Build < 20000)
-            {
-                WindowBorder.CornerRadius = new CornerRadius(0);
-            }
-            else
-            {
-                // For Rounded Window Corners and Shadows
-                // src : https://docs.microsoft.com/en-us/windows/apps/desktop/modernize/apply-rounded-corners#example-1---rounding-an-apps-main-window-in-c---wpf
-                var preference = DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_ROUND;
-                DwmSetWindowAttribute(
-                    new WindowInteropHelper(GetWindow(this)).EnsureHandle(),
-                    DWMWINDOWATTRIBUTE.DWMWA_WINDOW_CORNER_PREFERENCE,
-                    ref preference,
-                    sizeof(uint)
-                );
-            }
+            ShowBrowserIcons();
+        }
 
-            WindowChrome.SetWindowChrome(this, new WindowChrome()
-            {
-                CaptionHeight = 1
-            });
-
+        private void ShowBrowserIcons()
+        {
 #if DEBUG
             Stopwatch sw = new();
             sw.Start();
@@ -65,21 +47,8 @@ namespace Hurl.BrowserSelector
 
             foreach (Browser i in LoadableBrowsers)
             {
-                BrowserIconBtn browserUC = new BrowserIconBtn(i, OpenedLink);
-
-                var separator = new System.Windows.Shapes.Rectangle()
-                {
-                    Width = 2,
-                    Height = 40,
-                    Margin = new Thickness(3, 0, 3, 20),
-                    Fill = System.Windows.Media.Brushes.AliceBlue
-                };
-
-                _ = stacky.Children.Add(browserUC);
-                //_ = stacky.Children.Add(separator);
+                stacky.Children.Add(new BrowserIconBtn(i, OpenedLink));
             }
-
-            //stacky.Children.RemoveAt(stacky.Children.Count - 1);
         }
 
         public void init(string URL)
@@ -95,7 +64,7 @@ namespace Hurl.BrowserSelector
         {
             if (e.Key == Key.Escape)
             {
-                Close();
+                MinimizeWindow();
             }
         }
 
@@ -104,30 +73,14 @@ namespace Hurl.BrowserSelector
         private void Draggable(object sender, MouseButtonEventArgs e) => this.DragMove();
         private void CloseBtnClick(object sender, RoutedEventArgs e)
         {
+            MinimizeWindow();
+        }
+
+        private void MinimizeWindow()
+        {
             this.WindowState = WindowState.Minimized;
-            //this.Close();
         }
 
-        public enum DWMWINDOWATTRIBUTE
-        {
-            DWMWA_WINDOW_CORNER_PREFERENCE = 33
-        }
 
-        // The DWM_WINDOW_CORNER_PREFERENCE enum for DwmSetWindowAttribute's third parameter, which tells the function
-        // what value of the enum to set.
-        public enum DWM_WINDOW_CORNER_PREFERENCE
-        {
-            DWMWCP_DEFAULT = 0,
-            DWMWCP_DONOTROUND = 1,
-            DWMWCP_ROUND = 2,
-            DWMWCP_ROUNDSMALL = 3
-        }
-
-        // Import dwmapi.dll and define DwmSetWindowAttribute in C# corresponding to the native function.
-        [DllImport("dwmapi.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-        private static extern long DwmSetWindowAttribute(IntPtr hwnd,
-                                                         DWMWINDOWATTRIBUTE attribute,
-                                                         ref DWM_WINDOW_CORNER_PREFERENCE pvAttribute,
-                                                         uint cbAttribute);
     }
 }
