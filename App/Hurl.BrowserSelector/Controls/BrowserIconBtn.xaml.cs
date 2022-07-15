5642b0af3c524759eec27680caf3be865c180b7a
u-c-s <uchanakyasrinivas@gmail.com>
2022-07-16 01:01:19 +0530

    working additional opts btn (closes #36) - Need to improve the implementation

--- App/Hurl.BrowserSelector/Controls/BrowserIconBtn.xaml.cs
+++ App/Hurl.BrowserSelector/Controls/BrowserIconBtn.xaml.cs
@@ -56,5 +56,14 @@ namespace Hurl.BrowserSelector.Controls
             parent.WindowState = WindowState.Minimized;
             parent.Hide();
         }
+
+        private void AdditionalBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            var x = Resources["TheCM"] as ContextMenu;
+            x.PlacementTarget = btn;
+            x.IsOpen = true;
+            e.Handled = true;
+        }
     }
 }
