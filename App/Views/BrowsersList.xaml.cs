3abf43eea2f2a9281509b38b724f05f725da7cda
u-c-s <uchanakyasrinivas@gmail.com>
2022-08-04 08:43:59 +0530

    implement back the click browser icon to open link

--- App/Views/BrowsersList.xaml.cs
+++ App/Views/BrowsersList.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Hurl.BrowserSelector.Models;
 using Hurl.BrowserSelector.Views.ViewModels;
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Hurl.BrowserSelector.Views
@@ -12,6 +14,30 @@ namespace Hurl.BrowserSelector.Views
         public BrowsersList()
         {
             InitializeComponent();
+        }
+
+        private void BtnArea_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var tag = (sender as Border).Tag as Browser;
+            Process.Start(tag.ExePath, "https://github.com/u-c-s" + " " + tag.LaunchArgs);
+
+            //if (!string.IsNullOrEmpty(browser.LaunchArgs) && browser.LaunchArgs.Contains("%URL%"))
+            //{
+            //    var newArg = browser.LaunchArgs.Replace("%URL%", _currentLink.Url);
+            //    Process.Start(browser.ExePath, newArg);
+            //}
+            //else
+            //{
+            //    Process.Start(browser.ExePath, _currentLink.Url + " " + browser.LaunchArgs);
+            //}
+            MinimizeWindow();
+        }
+
+        private void MinimizeWindow()
+        {
+            var parent = Window.GetWindow(this);
+            parent.WindowState = WindowState.Minimized;
+            parent.Hide();
         }
     }
 }
