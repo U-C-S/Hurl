17a0cba115bb3af8eedc2104f7928458ab814efe
U-C-S <uchanakyasrinivas@gmail.com>
2022-12-12 11:06:56 +0530

    Add `Hurl.Settings` Project

---
+++ Settings/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Hurl.Settings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Wpf.Ui.Controls.UiWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
