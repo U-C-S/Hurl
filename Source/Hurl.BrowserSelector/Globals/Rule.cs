4d33995891c6c97bb8c8c8a918f94faa4a823772
U-C-S <uchanakyasrinivas@gmail.com>
2023-01-25 06:45:54 +0530

    Create Singleton `Rule`

---
+++ Source/Hurl.BrowserSelector/Globals/Rule.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Hurl.BrowserSelector.Globals
+{
+    internal class Rule
+    {
+        private static Rule _instance = null;
+
+        private string _rule;
+
+        public string Rulee
+        {
+            get => _rule;
+            set
+            {
+                if (value != _rule)
+                {
+                    _rule = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public static string Value
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Rule();
+                }
+                return _instance.Rulee;
+            }
+            set
+            {
+                if (_instance == null)
+                {
+                    _instance = new Rule();
+                }
+                _instance.Rulee = value;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
