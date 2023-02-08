0903f1e5c83520c2955d31fda64f3cec60571ae5
U-C-S <uchanakyasrinivas@gmail.com>
2023-02-08 17:29:46 +0530

    working page change in hurl settings

--- Source/Hurl.Settings/MainWindow.xaml.cs
+++ Source/Hurl.Settings/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Hurl.Settings.Pages;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Controls;
 
 namespace Hurl.Settings
 {
@@ -10,11 +14,25 @@ namespace Hurl.Settings
         public MainWindow()
         {
             InitializeComponent();
-            //ContentBox.Children.Add(new Page1());
         }
+
+        public Dictionary<object, object> Pages = new();
 
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            object x = (sender as Button).Tag;
+            if (Pages.ContainsKey(x))
+            {
+                ContentBox.Child = (System.Windows.UIElement)Pages[x];
+                Debug.WriteLine("Used existing instance");
+            }
+            else
+            {
+                object instance = Activator.CreateInstance((System.Type)x);
+                Pages.Add(x, instance);
+                ContentBox.Child = (System.Windows.UIElement)instance;
+                Debug.WriteLine($"New instance for the key: {x}");
+            }
         }
     }
 }
