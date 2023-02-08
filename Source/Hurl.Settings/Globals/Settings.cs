d05cd197aa4fdafc409466992644873bc519977e
U-C-S <uchanakyasrinivas@gmail.com>
2023-02-08 13:04:12 +0530

    Ref Hurl.Library, copy SettingsGlobal to Hurl.Settings

---
+++ Source/Hurl.Settings/Globals/Settings.cs
@@ -0,0 +1,71 @@
+﻿using Hurl.Library;
+using Hurl.Library.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace Hurl.Settings.Globals
+{
+    public class SettingsGlobal
+    {
+        private static SettingsGlobal _instance = new();
+
+        private Library.Models.Settings _settings = SettingsFile.GetSettings();
+
+        public static Library.Models.Settings Value
+        {
+            get
+            {
+                return _instance._settings;
+            }
+            set
+            {
+                _instance._settings = value;
+            }
+        }
+
+        public static void AddBrowserRule(string rule, int browserIndex)
+        {
+            Value.Browsers[browserIndex].Rules.Append(rule);
+
+            Save();
+        }
+
+        public static void AddBrowserRule(string rule, Browser browser)
+        {
+            //use linq to find the browser in the browser list
+            var x = Value.Browsers.FindIndex(b => b.Name == browser.Name);
+            Value.Browsers[x].Rules.Append(rule);
+
+            Save();
+        }
+
+        public static void AdjustWindowSize(SizeChangedEventArgs e)
+        {
+            if (e.PreviousSize.Width != 0)
+            {
+                if (Value.AppSettings != null)
+                {
+                    Value.AppSettings.WindowSize = new int[] { (int)e.NewSize.Width, (int)e.NewSize.Height };
+                }
+                else
+                {
+                    Value.AppSettings = new AppSettings() { WindowSize = new int[] { (int)e.NewSize.Width, (int)e.NewSize.Height } };
+                }
+
+            }
+
+            Save();
+        }
+
+        public static List<Browser> GetBrowsers()
+        {
+            return Library.GetBrowsers.FromSettingsFile(Value, false);
+        }
+
+        private static void Save()
+        {
+            JsonOperations.FromModelToJson(_instance._settings, Constants.APP_SETTINGS_MAIN);
+        }
+    }
+}
