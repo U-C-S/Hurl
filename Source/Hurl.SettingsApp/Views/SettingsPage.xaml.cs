3b4a7a33cbd265ddf849bfd03c3325464bc218c8
U-C-S <uchanakyasrinivas@gmail.com>
2023-04-18 09:58:18 +0530

    Working AppSettings controls

--- Source/Hurl.SettingsApp/Views/SettingsPage.xaml.cs
+++ Source/Hurl.SettingsApp/Views/SettingsPage.xaml.cs
@@ -15,6 +15,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Hurl.SettingsApp.ViewModels;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -31,5 +32,7 @@ namespace Hurl.SettingsApp.Views
             this.InitializeComponent();
             NavigationCacheMode = NavigationCacheMode.Required;
         }
+
+        public SettingsViewModel ViewModel => new();
     }
 }
