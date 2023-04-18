113e7e696f213e911830a1f17a275addf0be32fa
U-C-S <uchanakyasrinivas@gmail.com>
2023-04-18 08:02:29 +0530

    Initial Implemention of Item Navigation

--- Source/Hurl.SettingsApp/MainWindow.xaml.cs
+++ Source/Hurl.SettingsApp/MainWindow.xaml.cs
@@ -1,20 +1,12 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using CommunityToolkit.WinUI.UI;
+using Hurl.SettingsApp.ViewModels;
+using Hurl.SettingsApp.Views;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Graphics;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -39,9 +31,38 @@ namespace Hurl.SettingsApp
             this.SystemBackdrop = new MicaBackdrop();
         }
 
-        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
-        {
+        //public static MainWindowViewModel ViewModel => new();
 
+        private void OnNavItemClicked(object sender, ItemClickEventArgs e)
+        {
+            if (e.ClickedItem is FrameworkElement f && f.FindParent<ListViewItem>() is { Tag: string tag })
+            {
+
+                switch (tag)
+                {
+                    case "home":
+                        NavigationFrame.Navigate(typeof(HomePage));
+                        break;
+                    case "apps":
+                        //ViewModel.Navigate(ContentPageType.Apps);
+                        break;
+                    case "rules":
+                        break;
+                    case "settings":
+                        NavigationFrame.Navigate(typeof(SettingsPage)); ;
+                        break;
+                }
+
+                if (tag == "settings")
+                {
+                    NavMenuFooterList.SelectedIndex = 0;
+                    NavMenuHeaderList.SelectedIndex = -1;
+                }
+                else
+                {
+                    NavMenuFooterList.SelectedIndex = -1;
+                }
+            }
         }
     }
 }
