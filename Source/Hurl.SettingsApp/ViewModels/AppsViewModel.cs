cb4a4ae22f6316266d0bfadb3d69f05400389c48
U-C-S <uchanakyasrinivas@gmail.com>
2023-07-16 15:48:48 +0530

    update app page vm and design

---
+++ Source/Hurl.SettingsApp/ViewModels/AppsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Hurl.Library.Models;
+using System.Collections.Generic;
+
+namespace Hurl.SettingsApp.ViewModels
+{
+    public class AppsViewModel
+    {
+        private List<Browser> jsonData { get; set; }
+
+        public AppsViewModel()
+        {
+            jsonData = State.Settings.GetBrowsers();
+        }
+
+        public List<Browser> BrowsersList { get => jsonData; }
+    }
+}
