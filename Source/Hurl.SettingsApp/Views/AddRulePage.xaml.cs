172b9f5196bd20092859d53346455d851b411eb1
U-C-S <uchanakyasrinivas@gmail.com>
2023-11-13 12:17:12 +0530

    Create new rule now adds a NewRuleCard into stackpanel

--- Source/Hurl.SettingsApp/Views/AddRulePage.xaml.cs
+++ Source/Hurl.SettingsApp/Views/AddRulePage.xaml.cs
@@ -1,3 +1,4 @@
+using Hurl.SettingsApp.Controls;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -26,6 +27,11 @@ namespace Hurl.SettingsApp.Views
         public AddRulePage()
         {
             this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RulesStack.Children.Add(new NewRuleCard());
         }
     }
 }
