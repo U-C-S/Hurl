6db9d18c351b8847411b0e8b899bb04363590621
U-C-S <uchanakyasrinivas@gmail.com>
2023-12-24 08:16:08 +0530

    Sample code for get cmd args in Settings

--- Source/Hurl.Settings/App.xaml.cs
+++ Source/Hurl.Settings/App.xaml.cs
@@ -1,69 +1,50 @@
 ﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace Hurl.Settings
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
     public partial class App : Application
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
             this.InitializeComponent();
         }
 
-        /// <summary>
-        /// Invoked when the application is launched.
-        /// </summary>
-        /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
+            var cmdArgs = Environment.GetCommandLineArgs();
+
             m_window = new MainWindow();
-            m_window.Activate();
+
+            if (cmdArgs.Length > 1)
+            {
+                ProcessArgs(cmdArgs);
+                m_window.Activate();
+            }
+            else
+            {
+                m_window.Activate();
+            }
+
         }
 
         private Window m_window;
 
-        void processArgs(string[] args)
+        void ProcessArgs(string[] args)
         {
-            if (args.Length > 0)
-            {
-                var primaryArg = args[0];
+            Debug.WriteLine(args[0]);
+            var primaryArg = args[1];
 
-                if (primaryArg.StartsWith("--newrule"))
+            if (primaryArg.StartsWith("--newrule"))
+            {
+                if (args.Length >= 2)
                 {
-                    if (args.Length >= 2)
-                    {
-                        var ruleURL = args[1];
-                        Debug.WriteLine($"Creating new rule for {ruleURL}");
-                        m_window.Title = $"Hurl Settings - New Rule for {ruleURL}";
-                    }
+                    var ruleURL = args[2];
+                    Debug.WriteLine($"------------------------> Creating new rule for {ruleURL}");
                 }
             }
+
         }
     }
 }
