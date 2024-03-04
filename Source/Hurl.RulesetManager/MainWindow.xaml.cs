7c329eb7ee450c1c2e7a6ea75b7c3125df66ca4a
Chanakya <uchanakyasrinivas@gmail.com>
2024-03-04 23:45:32 +0530

    Init UI

--- Source/Hurl.RulesetManager/MainWindow.xaml.cs
+++ Source/Hurl.RulesetManager/MainWindow.xaml.cs
@@ -23,5 +23,42 @@ namespace Hurl.RulesetManager
 
             //Loaded += (sender, args) => SystemThemeWatcher.Watch(this, Wpf.Ui.Controls.WindowBackdropType.Mica, true);
         }
+
+        //public QuickRuleAddWindow()
+        //{
+        //    InitializeComponent();
+
+        //    var x = SettingsGlobal.GetBrowserNameList();
+        //    x.Add("_Hurl");
+        //    DataContext = x;
+        //}
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedBrowser = TargetBrowser.SelectedValue;
+            var rules = RuleInput.Text;
+            if (selectedBrowser == null || string.IsNullOrEmpty(rules))
+            {
+                WarnText.Visibility = Visibility.Visible;
+                WarnText.Height = 20;
+                return;
+            };
+
+            var rulesList = new List<string>();
+
+            foreach (var rule in rules.Split('|'))
+            {
+                rulesList.Add(rule.Trim());
+            };
+
+            //SettingsGlobal.AddBrowserRule(rulesList, TargetBrowser.SelectedValue.ToString());
+
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) => this.Close();
     }
+
+
+
 }
