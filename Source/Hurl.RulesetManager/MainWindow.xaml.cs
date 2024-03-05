486cea9c73ac5ad63d448d81df3e29ac316a1a80
Chanakya <uchanakyasrinivas@gmail.com>
2024-03-06 03:26:54 +0530

    Redesign Ruleset Page - Move Ruleset editing to its own window

--- Source/Hurl.RulesetManager/MainWindow.xaml.cs
+++ Source/Hurl.RulesetManager/MainWindow.xaml.cs
@@ -10,55 +10,17 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Wpf.Ui.Appearance;
 
-namespace Hurl.RulesetManager
+namespace Hurl.RulesetManager;
+
+public partial class MainWindow
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
+        InitializeComponent();
 
-            //Loaded += (sender, args) => SystemThemeWatcher.Watch(this, Wpf.Ui.Controls.WindowBackdropType.Mica, true);
-        }
-
-        //public QuickRuleAddWindow()
-        //{
-        //    InitializeComponent();
-
-        //    var x = SettingsGlobal.GetBrowserNameList();
-        //    x.Add("_Hurl");
-        //    DataContext = x;
-        //}
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var selectedBrowser = TargetBrowser.SelectedValue;
-            var rules = "RuleInput.Text";
-            if (selectedBrowser == null || string.IsNullOrEmpty(rules))
-            {
-                WarnText.Visibility = Visibility.Visible;
-                WarnText.Height = 20;
-                return;
-            };
-
-            var rulesList = new List<string>();
-
-            foreach (var rule in rules.Split('|'))
-            {
-                rulesList.Add(rule.Trim());
-            };
-
-            //SettingsGlobal.AddBrowserRule(rulesList, TargetBrowser.SelectedValue.ToString());
-
-            this.Close();
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e) => this.Close();
+        //Loaded += (sender, args) => SystemThemeWatcher.Watch(this, Wpf.Ui.Controls.WindowBackdropType.Mica, true);
+        DataContext = Hurl.Library.SettingsFile.GetSettings().Rulesets;
     }
 
-
-
+    private void Button_Click_1(object sender, RoutedEventArgs e) => this.Close();
 }
---
+++ Source/Hurl.RulesetManager/Windows/EditRuleset.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hurl.RulesetManager.Windows;
+
+public partial class EditRuleset
+{
+    public EditRuleset()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var selectedBrowser = TargetBrowser.SelectedValue;
+        var rules = "RuleInput.Text";
+        if (selectedBrowser == null || string.IsNullOrEmpty(rules))
+        {
+            WarnText.Visibility = Visibility.Visible;
+            WarnText.Height = 20;
+            return;
+        };
+
+        var rulesList = new List<string>();
+
+        foreach (var rule in rules.Split('|'))
+        {
+            rulesList.Add(rule.Trim());
+        };
+
+        //SettingsGlobal.AddBrowserRule(rulesList, TargetBrowser.SelectedValue.ToString());
+
+        this.Close();
+    }
+}
+
