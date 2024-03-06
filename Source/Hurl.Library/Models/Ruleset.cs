93dacdbccf918512c341d943ec3a5b5aa25be6d6
Chanakya <uchanakyasrinivas@gmail.com>
2024-03-06 23:48:30 +0530

    Add `Rule` class and `RuleMode` Enum

--- Source/Hurl.Library/Models/Ruleset.cs
+++ Source/Hurl.Library/Models/Ruleset.cs
@@ -16,3 +16,62 @@ public class Ruleset
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public int AltLaunchIndex { get; set; }
 }
+
+
+public enum RuleMode
+{
+    Domain,
+    String,
+    Regex,
+    Glob
+}
+
+
+public class Rule
+{
+    public Rule(string RuleContent, string Mode)
+    {
+        this.RuleContent = RuleContent;
+        this.Mode = Mode switch
+        {
+            "Domain" => RuleMode.Domain,
+            "Regex" => RuleMode.Regex,
+            "Glob" => RuleMode.Glob,
+            _ => RuleMode.String
+        };
+    }
+
+    public Rule(string storedRule)
+    {
+        var split = storedRule.Split('$', 2);
+        var modeLetter = split[0];
+        var content = split[1];
+
+        this.RuleContent = content;
+        this.Mode = modeLetter switch
+        {
+            "d" => RuleMode.Domain,
+            "r" => RuleMode.Regex,
+            "g" => RuleMode.Glob,
+            _ => RuleMode.String
+        };
+    }
+
+    public string RuleContent { get; set; }
+
+    public RuleMode Mode { get; set; }
+
+    public override string ToString()
+    {
+        string RuleString = Mode switch
+        {
+            RuleMode.Domain => $"d${RuleContent}",
+            RuleMode.String => $"{RuleContent}",
+            RuleMode.Regex => $"r${RuleContent}",
+            RuleMode.Glob => $"g${RuleContent}",
+            _ => throw new NotImplementedException()
+        };
+
+        return RuleString;
+    }
+}
