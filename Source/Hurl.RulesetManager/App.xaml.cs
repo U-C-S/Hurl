adbbd0afc596f821dbd38560c93ff9c8ab337df2
Chanakya <uchanakyasrinivas@gmail.com>
2024-03-17 10:42:17 +0530

    Error and Crash message box

--- Source/Hurl.RulesetManager/App.xaml.cs
+++ Source/Hurl.RulesetManager/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Hurl.RulesetManager.Windows;
+using System.Text.Json;
 using System.Windows;
 
 namespace Hurl.RulesetManager;
@@ -9,5 +10,25 @@ public partial class App : Application
     {
         MainWindow mainWindow = new();
         mainWindow.Show();
+    }
+
+    private void Dispatcher_UnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+    {
+        string ErrorMsgBuffer;
+        string ErrorWndTitle;
+        switch (e.Exception?.InnerException)
+        {
+            case JsonException:
+                ErrorMsgBuffer = "The UserSettings.json file is in invalid JSON format. \n";
+                ErrorWndTitle = "Invalid JSON";
+                break;
+            default:
+                ErrorMsgBuffer = "An unknown error has occurred. \n";
+                ErrorWndTitle = "Unknown Error";
+                break;
+
+        }
+        string errorMessage = string.Format("{0}\n{1}\n\n{2}", ErrorMsgBuffer, e.Exception?.InnerException?.Message, e.Exception.Message);
+        MessageBox.Show(errorMessage, ErrorWndTitle, MessageBoxButton.OK, MessageBoxImage.Error);
     }
 }
