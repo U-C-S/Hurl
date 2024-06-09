75319d5057be2d2919c13ef335dc6a6ec67bc894
Chanakya <uchanakyasrinivas@gmail.com>
2024-06-09 21:38:26 +0530

    Get hurl_exe_path and open exe from launcher

--- Source/Launcher/src/main.rs
+++ Source/Launcher/src/main.rs
@@ -1,29 +1,39 @@
 #![windows_subsystem = "windows"]
 
-use std::env;
+use std::{env, process::Command};
 
 use tokio::net::windows::named_pipe::ClientOptions;
 
 mod models;
 
-// use windows::{core::*, Win32::System::Pipes::CallNamedPipeA};
-
-const USER_SETTINGS: &str = "C:\\Users\\uchan\\AppData\\Roaming\\Hurl\\UserSettings.json";
-const PIPE_NAME: &str = r"\\.\pipe\HurlNamedPipe";
+static USER_SETTINGS: &str = "C:\\Users\\uchan\\AppData\\Roaming\\Hurl\\UserSettings.json";
+static PIPE_NAME: &str = r"\\.\pipe\HurlNamedPipe";
 
 #[tokio::main]
 async fn main() {
+    let hurl_exe_path = {
+        let current_exe_path = env::current_exe().unwrap();
+        let current_dir = current_exe_path.parent().unwrap();
+        current_dir.join("Hurl.exe")
+    };
+
     let args: Vec<String> = env::args().collect();
+    let args_str = serde_json::to_string(&args).unwrap();
 
     // let file = std::fs::read_to_string(USER_SETTINGS).unwrap();
     // let user_settings: models::Settings = serde_json::from_str(&file).unwrap();
     // let user_settings_json = serde_json::to_string(&user_settings).unwrap();
 
-    print!("Waiting for server to be ready...");
-    let client = ClientOptions::new().open(PIPE_NAME).unwrap();
-
-    client.writable().await.unwrap();
-    client.try_write(serde_json::to_string(&args).unwrap().as_bytes()).ok();
-
-    println!("{:?}", args);
+    let pipe_conn = ClientOptions::new().open(PIPE_NAME);
+    match pipe_conn {
+        Ok(client) => {
+            print!("Connected to the server");
+            client.writable().await.unwrap();
+            let _ = client.try_write(args_str.as_bytes());
+        }
+        Err(_) => {
+            print!("Failed to connect to the server");
+            let _ = Command::new(hurl_exe_path).args(args).spawn();
+        }
+    }
 }
