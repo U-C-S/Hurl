5725a3e7f7f2cc600b5bf376e4467c2f553d5602
Chanakya <uchanakyasrinivas@gmail.com>
2024-06-11 23:49:26 +0530

    Update `install-nmh.ps1` script

--- Extensions/install-nmh.ps1
+++ Extensions/install-nmh.ps1
@@ -1,4 +1,25 @@
-$CURRENT_DIR = Get-Location
-$PATH_TO_NMHOST = "$CURRENT_DIR\nmh-manifest.json"
+$EXTENSION_ID = $args[0]
+
+$CURRENT_DIR = $args[1]
+if ($CURRENT_DIR -eq $null) {
+  $CURRENT_DIR = Get-Location
+}
+$CURRENT_DIR = "$CURRENT_DIR".Replace('\', '/')
+
+$PATH_TO_NMHOST = "$CURRENT_DIR/nmh-manifest.json"
+Write-Output "Creating Native Messaging Host manifest file at $PATH_TO_NMHOST"
+
+
+$NMH_MANIFEST = @"
+{
+  "name": "com.3721tools.hurl",
+  "description": "Hurl Proxy Native Messaging Host",
+  "path": "$CURRENT_DIR/Laucher.exe",
+  "type": "stdio",
+  "allowed_origins": ["chrome-extension://$EXTENSION_ID/"]
+}
+"@
+
+$NMH_MANIFEST | Out-File -FilePath $PATH_TO_NMHOST
 
 REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.3721tools.hurl" /ve /t REG_SZ /d "$PATH_TO_NMHOST" /f
