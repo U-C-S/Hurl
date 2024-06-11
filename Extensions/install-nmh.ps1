814c5a496b8047a850632bbd2740e72bc6f13b6c
Chanakya <uchanakyasrinivas@gmail.com>
2024-06-12 01:14:08 +0530

    Add firefox vars in install-nmh.ps1 script

--- Extensions/install-nmh.ps1
+++ Extensions/install-nmh.ps1
@@ -1,5 +1,6 @@
 $EXTENSION_ID = $args[0]
 
+# Custom path to the directory. Absolute path is required.
 $CURRENT_DIR = $args[1]
 if ($CURRENT_DIR -eq $null) {
   $CURRENT_DIR = Get-Location
@@ -20,6 +21,19 @@ $NMH_MANIFEST = @"
 }
 "@
 
+# $FIREFOX_NMH_MANIFEST = @"
+# {
+#   "name": "com.3721tools.hurl",
+#   "description": "Hurl Proxy Native Messaging Host",
+#   "path": "$CURRENT_DIR/Launcher.exe",
+#   "type": "stdio",
+#   "allowed_extensions": ["chrome-extension://$EXTENSION_ID/"]
+# }
+# "@
+
 $NMH_MANIFEST | Out-File -FilePath $PATH_TO_NMHOST
 
-REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.3721tools.hurl" /ve /t REG_SZ /d "$PATH_TO_NMHOST" /f
+$REG_PATH = "HKCU\Software\Google\Chrome\NativeMessagingHosts"
+# $FIREFOX_REG_PATH = "HKCU\Software\Mozilla\NativeMessagingHosts"
+
+REG ADD "$REG_PATH\com.3721tools.hurl" /ve /t REG_SZ /d "$PATH_TO_NMHOST" /f
