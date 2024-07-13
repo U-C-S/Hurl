16f8df07fbf0139de48abbd34cdbcc0b845473b4
Chanakya <uchanakyasrinivas@gmail.com>
2024-07-14 00:35:49 +0530

    Minor tweaks and fixes

--- Source/Hurl.BrowserSelector/Controls/BrowserButton.xaml.cs
+++ Source/Hurl.BrowserSelector/Controls/BrowserButton.xaml.cs
@@ -2,21 +2,10 @@
 using Hurl.BrowserSelector.Globals;
 using Hurl.BrowserSelector.Helpers;
 using Hurl.Library.Models;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Hurl.BrowserSelector.Controls
 {
@@ -39,8 +28,8 @@ namespace Hurl.BrowserSelector.Controls
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            var alt = ((MenuItem)sender).Tag as AltLaunchParentConverter.AltLaunchParent;
-            OpenAltLaunch(alt.AltLaunch, alt.Browser);
+            var alt = ((MenuItem)sender).Tag as AlternateLaunch;
+            OpenAltLaunch(alt, (Browser)DataContext);
             MinimizeWindow();
         }
 
