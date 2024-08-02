41d952ea661bdcef409dcc3c62443ddddd7d154b
Chanakya <uchanakyasrinivas@gmail.com>
2024-08-03 03:00:39 +0530

    Simplify `NewRuleCard` code

--- Source/Hurl.Settings/Controls/NewRuleCard.xaml.cs
+++ Source/Hurl.Settings/Controls/NewRuleCard.xaml.cs
@@ -1,46 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
+using Hurl.Library.Models;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using System;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace Hurl.Settings.Controls
+namespace Hurl.Settings.Controls;
+
+public sealed partial class NewRuleCard : UserControl
 {
-    public sealed partial class NewRuleCard : UserControl
+    public NewRuleCard()
     {
-        public NewRuleCard()
-        {
-            this.InitializeComponent();
-        }
+        InitializeComponent();
 
-        public string ConstructRule()
-        {
-            var ruleType = RuleTypeControl.SelectedValue;
-            var ruleValue = RuleValueControl.Text;
+        RuleTypeControl.ItemsSource = Enum.GetValues(typeof(RuleMode));
+    }
 
-            if (ruleType == null || string.IsNullOrEmpty(ruleValue))
-            {
-                return string.Empty;
-            }
+    public string ConstructRule()
+    {
+        var ruleType = RuleTypeControl.SelectedValue.ToString();
+        var ruleValue = RuleValueControl.Text;
 
-            return ruleType switch
-            {
-                "regex" => $"r${ruleValue}",
-                "glob" => $"g${ruleValue}",
-                _ => $"s${ruleValue}",
-            };
-        }
+        return new Rule(ruleValue, ruleType).ToString();
     }
 }
+
