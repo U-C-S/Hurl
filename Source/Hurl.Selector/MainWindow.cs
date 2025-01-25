c5727a086982c2f9d5cf9e94d2144bc10de6c9a7
U-C-S <uchanakyasrinivas@gmail.com>
2025-01-25 17:00:20 +0530

    Init UI

---
+++ Source/Hurl.Selector/MainWindow.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
+
+namespace Hurl.Selector;
+internal class MainWindow
+{
+    readonly Window window;
+    public MainWindow()
+    {
+        window = new Window()
+        {
+            SystemBackdrop = new MicaBackdrop(),
+            ExtendsContentIntoTitleBar = true,
+            Title = "Hurl Selector Preview"
+        };
+        window.AppWindow.ResizeClient(new Windows.Graphics.SizeInt32(500, 250));
+    }
+
+    public Window GetWindow => window;
+}
