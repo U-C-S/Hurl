68e5ad3da15cf09abd33cfc13e0c62b1ef9dd1cc
U-C-S <uchanakyasrinivas@gmail.com>
2025-06-28 19:02:10 +0530

    Add back fetching Browser Icon

--- Source/Hurl.Library/Models/Browser.cs
+++ Source/Hurl.Library/Models/Browser.cs
@@ -1,6 +1,9 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.ObjectModel;
+using System.Drawing;
 using System.Text.Json.Serialization;
+using System.Windows;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace Hurl.Library.Models;
@@ -33,9 +36,39 @@ public partial class Browser : ObservableObject
     [property: JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public bool hidden = false;
 
-    [ObservableProperty]
-    [property: JsonIgnore]
-    public BitmapImage? icon;
+    [JsonIgnore]
+    public ImageSource? Icon
+    {
+        get
+        {
+            if (!string.IsNullOrEmpty(CustomIconPath) && CustomIconPath.EndsWith(".ico"))
+            {
+                Icon RawIcon = new(CustomIconPath, -1, -1);
+                return IconUtilites.ToImageSource(RawIcon);
+            }
+            else if (!string.IsNullOrEmpty(CustomIconPath))
+            {
+                return new BitmapImage(new Uri(CustomIconPath));
+            }
+            else if (!string.IsNullOrEmpty(ExePath))
+            {
+                Icon? RawIcon = IconExtractor.FromFile(ExePath.Trim('"'));
+
+                return IconUtilites.ToImageSource(RawIcon);
+            }
+            else
+                return null;
+        }
+    }
+
+    [JsonIgnore]
+    public Visibility ShowAdditionalBtn
+    {
+        get
+        {
+            return AlternateLaunches == null || AlternateLaunches.Count == 0 ? Visibility.Hidden : Visibility.Visible;
+        }
+    }
 }
 
 public partial class AlternateLaunch(string ItemName, string LaunchArgs) : ObservableObject
