eca5d4498d42de5d0bf5040acdcfa3d8a3de4169
U-C-S <uchanakyasrinivas@gmail.com>
2025-08-29 23:06:06 +0530

    Add timely service

---
+++ Source/Hurl.BrowserSelector/Services/TimelyService.cs
@@ -0,0 +1,69 @@
+﻿using Hurl.Library;
+using Hurl.Library.Models;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Hurl.BrowserSelector.Services;
+
+internal class TimelyService
+{
+    public static bool CheckAndLaunch(string url)
+    {
+        var Path_TempDef = Path.Combine(Constants.APP_SETTINGS_DIR, "TempDefault.json");
+        if (File.Exists(Path_TempDef))
+        {
+            var obj = JsonOperations.FromJsonToModel<TemporaryDefaultBrowser>(Path_TempDef);
+            if (obj?.ValidTill >= DateTime.Now)
+            {
+                Process.Start(obj.TargetBrowser.ExePath, url);
+                Debug.WriteLine(obj.TargetBrowser.ExePath);
+                return true;
+            }
+            else
+            {
+                File.Delete(Path_TempDef);
+            }
+        }
+        return false;
+    }
+
+    public static void Create(int mins, Browser browser)
+    {
+        var dataObject = new TemporaryDefaultBrowser()
+        {
+            TargetBrowser = browser,
+            SelectedAt = DateTime.Now,
+            ValidTill = DateTime.Now.AddMinutes(mins)
+        };
+
+        JsonOperations.FromModelToJson(dataObject, Path.Combine(Constants.APP_SETTINGS_DIR, "TempDefault.json"));
+    }
+
+    public static TemporaryDefaultBrowser? Get()
+    {
+        var Path_TempDef = Path.Combine(Constants.APP_SETTINGS_DIR, "TempDefault.json");
+        if (File.Exists(Path_TempDef))
+        {
+            var obj = JsonOperations.FromJsonToModel<TemporaryDefaultBrowser>(Path_TempDef);
+            if (obj?.ValidTill >= DateTime.Now)
+            {
+                return obj;
+            }
+            else
+            {
+                File.Delete(Path_TempDef);
+            }
+        }
+        return null;
+    }
+
+    internal static void DeleteCurrent()
+    {
+        var Path_TempDef = Path.Combine(Constants.APP_SETTINGS_DIR, "TempDefault.json");
+        if (File.Exists(Path_TempDef))
+        {
+            File.Delete(Path_TempDef);
+        }
+    }
+}
