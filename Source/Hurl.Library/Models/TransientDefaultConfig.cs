6d87b7883be924526bb0316b789b0b6200ab1d54
U-C-S <uchanakyasrinivas@gmail.com>
2026-09-26 20:42:41 +0530

    Rename to TransientDefaultConfig

---
+++ Source/Hurl.Library/Models/TransientDefaultConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace Hurl.Library.Models;
+
+
+public record TransientBrowser(
+    Guid BrowserId,
+    Guid? AltLaunchId,
+    string Name,
+    string ExePath,
+    string Arguments);
+
+public class TransientDefaultConfig
+{
+    public required TransientBrowser Browser { get; set; }
+    
+    public required DateTime ValidTill { get; set; }
+
+    public DateTime SelectedAt { get; set; }
+}
